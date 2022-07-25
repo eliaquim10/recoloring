d9877941ff8b74a3639d278e1dd5f120449cccd4
eliaquim_samsung <emn3@cin.ufpe.br>
2022-07-25 15:21:36 -0300

    Remove de arquivos desnecessarios e altera o tamanho do batch

--- InstColorization/scripts/train.sh
+++ InstColorization/scripts/train.sh
@@ -3,12 +3,12 @@ DATASET_DIR=train_data/painting
 # Stage 1: Training Full Image Colorization
 mkdir -p checkpoints/coco_full
 cp checkpoints/siggraph_retrained/latest_net_G.pth checkpoints/coco_full/
-python train.py --stage full --name coco_full --sample_p 1.0 --niter 100 --niter_decay 50 --load_model --lr 0.0005 --model train --fineSize 256 --batch_size 16 --display_ncols 3 --display_freq 1600 --print_freq 1600 --train_img_dir $DATASET_DIR
+python train.py --stage full --name coco_full --sample_p 1.0 --niter 100 --niter_decay 50 --load_model --lr 0.0005 --model train --fineSize 256 --batch_size 8 --display_ncols 3 --display_freq 1600 --print_freq 1600 --train_img_dir $DATASET_DIR
 
 # Stage 2: Training Instance Image Colorization
 mkdir -p checkpoints/coco_instance
 cp checkpoints/coco_full/latest_net_G.pth checkpoints/coco_instance/
-python train.py --stage instance --name coco_instance --sample_p 1.0 --niter 100 --niter_decay 50 --load_model --lr 0.0005 --model train --fineSize 256 --batch_size 16 --display_ncols 3 --display_freq 1600 --print_freq 1600 --train_img_dir $DATASET_DIR
+python train.py --stage instance --name coco_instance --sample_p 1.0 --niter 100 --niter_decay 50 --load_model --lr 0.0005 --model train --fineSize 256 --batch_size 8 --display_ncols 3 --display_freq 1600 --print_freq 1600 --train_img_dir $DATASET_DIR
 
 # Stage 3: Training Fusion Module
 mkdir -p checkpoints/coco_mask
